e5f844c29852ecd8d438a6f05d6a3ccd84e4ea77
sachinrajc <sachinrajc7@gmail.com>
2024-07-31 19:04:28 -0400

    1commit

---
+++ scripts/app.js
@@ -0,0 +1,18 @@
+// Create a new Vue app
+const app = Vue.createApp({
+    // Define the data for the app
+    data() {
+        return {
+
+        };
+    },
+    // Define the methods for the app
+    methods: {
+        isActive(level) {
+            // Console.log level here to see the data structure, then check pressure against the current level and return TRUE or FALSE boolean
+        }
+    }
+});
+
+// Mount the app to the #app element
+app.mount('#app');
